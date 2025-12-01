15722aff052863a334be47a3a135b69080f6bd99
Nhasly.Dorado <dnhasly@gmail.com>
2025-12-01 13:10:11 -0500

    Cuarto capitulo

--- nextjs-dashboard/app/layout.tsx
+++ nextjs-dashboard/app/layout.tsx
@@ -1,8 +1,14 @@
-import { Inter, Lusitana } from 'next/font/google';
+import '@/app/ui/global.css';
+import { inter } from '@/app/ui/fonts';
  
-export const inter = Inter({ subsets: ['latin'] });
- 
-export const lusitana = Lusitana({
-  weight: ['400', '700'],
-  subsets: ['latin'],
-});
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={`${inter.className} antialiased`}>{children}</body>
+    </html>
+  );
+}
